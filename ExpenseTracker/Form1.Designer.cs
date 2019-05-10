b545e1d83cb1e932b8021a0569da6135a8aa83dc
M S <syedmashoodahmedshah@hotmail.com>
2019-05-10 08:22:10 +0100

    Frontend Expenses done

--- ExpenseTracker/Form1.Designer.cs
+++ ExpenseTracker/Form1.Designer.cs
@@ -28,20 +28,396 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lbl_income = new System.Windows.Forms.Label();
+            this.lbl_rm = new System.Windows.Forms.Label();
+            this.lbl_electricity = new System.Windows.Forms.Label();
+            this.lbl_hvi = new System.Windows.Forms.Label();
+            this.lbl_water = new System.Windows.Forms.Label();
+            this.lbl_ct = new System.Windows.Forms.Label();
+            this.lbl_mr = new System.Windows.Forms.Label();
+            this.lbl_pc = new System.Windows.Forms.Label();
+            this.lbl_cs = new System.Windows.Forms.Label();
+            this.lbl_parking = new System.Windows.Forms.Label();
+            this.lbl_fuel = new System.Windows.Forms.Label();
+            this.lbl_food = new System.Windows.Forms.Label();
+            this.lbl_entertainment = new System.Windows.Forms.Label();
+            this.lbl_sr = new System.Windows.Forms.Label();
+            this.lbl_travel = new System.Windows.Forms.Label();
+            this.Gifts = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.lbl_other = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lbl_income
+            // 
+            this.lbl_income.AutoSize = true;
+            this.lbl_income.Location = new System.Drawing.Point(13, 53);
+            this.lbl_income.Name = "lbl_income";
+            this.lbl_income.Size = new System.Drawing.Size(42, 13);
+            this.lbl_income.TabIndex = 0;
+            this.lbl_income.Text = "Income";
+            // 
+            // lbl_rm
+            // 
+            this.lbl_rm.AutoSize = true;
+            this.lbl_rm.Location = new System.Drawing.Point(13, 79);
+            this.lbl_rm.Name = "lbl_rm";
+            this.lbl_rm.Size = new System.Drawing.Size(80, 13);
+            this.lbl_rm.TabIndex = 1;
+            this.lbl_rm.Text = "Rent/Mortgage";
+            // 
+            // lbl_electricity
+            // 
+            this.lbl_electricity.AutoSize = true;
+            this.lbl_electricity.Location = new System.Drawing.Point(13, 105);
+            this.lbl_electricity.Name = "lbl_electricity";
+            this.lbl_electricity.Size = new System.Drawing.Size(52, 13);
+            this.lbl_electricity.TabIndex = 2;
+            this.lbl_electricity.Text = "Electricity";
+            // 
+            // lbl_hvi
+            // 
+            this.lbl_hvi.AutoSize = true;
+            this.lbl_hvi.Location = new System.Drawing.Point(13, 131);
+            this.lbl_hvi.Name = "lbl_hvi";
+            this.lbl_hvi.Size = new System.Drawing.Size(144, 13);
+            this.lbl_hvi.TabIndex = 3;
+            this.lbl_hvi.Text = "Home and Vehicle Insurance";
+            // 
+            // lbl_water
+            // 
+            this.lbl_water.AutoSize = true;
+            this.lbl_water.Location = new System.Drawing.Point(13, 157);
+            this.lbl_water.Name = "lbl_water";
+            this.lbl_water.Size = new System.Drawing.Size(36, 13);
+            this.lbl_water.TabIndex = 4;
+            this.lbl_water.Text = "Water";
+            // 
+            // lbl_ct
+            // 
+            this.lbl_ct.AutoSize = true;
+            this.lbl_ct.Location = new System.Drawing.Point(13, 183);
+            this.lbl_ct.Name = "lbl_ct";
+            this.lbl_ct.Size = new System.Drawing.Size(63, 13);
+            this.lbl_ct.TabIndex = 5;
+            this.lbl_ct.Text = "Council Tax";
+            // 
+            // lbl_mr
+            // 
+            this.lbl_mr.AutoSize = true;
+            this.lbl_mr.Location = new System.Drawing.Point(13, 209);
+            this.lbl_mr.Name = "lbl_mr";
+            this.lbl_mr.Size = new System.Drawing.Size(129, 13);
+            this.lbl_mr.TabIndex = 6;
+            this.lbl_mr.Text = "Maintenance and Repairs";
+            // 
+            // lbl_pc
+            // 
+            this.lbl_pc.AutoSize = true;
+            this.lbl_pc.Location = new System.Drawing.Point(13, 235);
+            this.lbl_pc.Name = "lbl_pc";
+            this.lbl_pc.Size = new System.Drawing.Size(73, 13);
+            this.lbl_pc.TabIndex = 7;
+            this.lbl_pc.Text = "Personal Care";
+            // 
+            // lbl_cs
+            // 
+            this.lbl_cs.AutoSize = true;
+            this.lbl_cs.Location = new System.Drawing.Point(13, 261);
+            this.lbl_cs.Name = "lbl_cs";
+            this.lbl_cs.Size = new System.Drawing.Size(96, 13);
+            this.lbl_cs.TabIndex = 8;
+            this.lbl_cs.Text = "Clothes and Shoes";
+            // 
+            // lbl_parking
+            // 
+            this.lbl_parking.AutoSize = true;
+            this.lbl_parking.Location = new System.Drawing.Point(13, 287);
+            this.lbl_parking.Name = "lbl_parking";
+            this.lbl_parking.Size = new System.Drawing.Size(43, 13);
+            this.lbl_parking.TabIndex = 9;
+            this.lbl_parking.Text = "Parking";
+            // 
+            // lbl_fuel
+            // 
+            this.lbl_fuel.AutoSize = true;
+            this.lbl_fuel.Location = new System.Drawing.Point(13, 313);
+            this.lbl_fuel.Name = "lbl_fuel";
+            this.lbl_fuel.Size = new System.Drawing.Size(27, 13);
+            this.lbl_fuel.TabIndex = 10;
+            this.lbl_fuel.Text = "Fuel";
+            // 
+            // lbl_food
+            // 
+            this.lbl_food.AutoSize = true;
+            this.lbl_food.Location = new System.Drawing.Point(13, 339);
+            this.lbl_food.Name = "lbl_food";
+            this.lbl_food.Size = new System.Drawing.Size(31, 13);
+            this.lbl_food.TabIndex = 11;
+            this.lbl_food.Text = "Food";
+            // 
+            // lbl_entertainment
+            // 
+            this.lbl_entertainment.AutoSize = true;
+            this.lbl_entertainment.Location = new System.Drawing.Point(13, 365);
+            this.lbl_entertainment.Name = "lbl_entertainment";
+            this.lbl_entertainment.Size = new System.Drawing.Size(72, 13);
+            this.lbl_entertainment.TabIndex = 12;
+            this.lbl_entertainment.Text = "Entertainment";
+            // 
+            // lbl_sr
+            // 
+            this.lbl_sr.AutoSize = true;
+            this.lbl_sr.Location = new System.Drawing.Point(13, 391);
+            this.lbl_sr.Name = "lbl_sr";
+            this.lbl_sr.Size = new System.Drawing.Size(121, 13);
+            this.lbl_sr.TabIndex = 13;
+            this.lbl_sr.Text = "Sports and Recreational";
+            // 
+            // lbl_travel
+            // 
+            this.lbl_travel.AutoSize = true;
+            this.lbl_travel.Location = new System.Drawing.Point(13, 417);
+            this.lbl_travel.Name = "lbl_travel";
+            this.lbl_travel.Size = new System.Drawing.Size(37, 13);
+            this.lbl_travel.TabIndex = 14;
+            this.lbl_travel.Text = "Travel";
+            // 
+            // Gifts
+            // 
+            this.Gifts.AutoSize = true;
+            this.Gifts.Location = new System.Drawing.Point(13, 443);
+            this.Gifts.Name = "Gifts";
+            this.Gifts.Size = new System.Drawing.Size(28, 13);
+            this.Gifts.TabIndex = 15;
+            this.Gifts.Text = "Gifts";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(261, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(261, 79);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 17;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(261, 105);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 18;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(261, 131);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 19;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(261, 157);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 20;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(261, 183);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 21;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(261, 209);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 22;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(261, 235);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 23;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(261, 261);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 24;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(261, 287);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 25;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(261, 313);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 26;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(261, 339);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 20);
+            this.textBox12.TabIndex = 27;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(261, 365);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(100, 20);
+            this.textBox13.TabIndex = 28;
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(261, 391);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(100, 20);
+            this.textBox14.TabIndex = 29;
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(261, 417);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(100, 20);
+            this.textBox15.TabIndex = 30;
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(261, 443);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(100, 20);
+            this.textBox16.TabIndex = 31;
+            // 
+            // textBox17
+            // 
+            this.textBox17.Location = new System.Drawing.Point(261, 469);
+            this.textBox17.Name = "textBox17";
+            this.textBox17.Size = new System.Drawing.Size(100, 20);
+            this.textBox17.TabIndex = 32;
+            // 
+            // lbl_other
+            // 
+            this.lbl_other.AutoSize = true;
+            this.lbl_other.Location = new System.Drawing.Point(16, 469);
+            this.lbl_other.Name = "lbl_other";
+            this.lbl_other.Size = new System.Drawing.Size(33, 13);
+            this.lbl_other.TabIndex = 33;
+            this.lbl_other.Text = "Other";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(831, 574);
+            this.ClientSize = new System.Drawing.Size(381, 506);
+            this.Controls.Add(this.lbl_other);
+            this.Controls.Add(this.textBox17);
+            this.Controls.Add(this.textBox16);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Gifts);
+            this.Controls.Add(this.lbl_travel);
+            this.Controls.Add(this.lbl_sr);
+            this.Controls.Add(this.lbl_entertainment);
+            this.Controls.Add(this.lbl_food);
+            this.Controls.Add(this.lbl_fuel);
+            this.Controls.Add(this.lbl_parking);
+            this.Controls.Add(this.lbl_cs);
+            this.Controls.Add(this.lbl_pc);
+            this.Controls.Add(this.lbl_mr);
+            this.Controls.Add(this.lbl_ct);
+            this.Controls.Add(this.lbl_water);
+            this.Controls.Add(this.lbl_hvi);
+            this.Controls.Add(this.lbl_electricity);
+            this.Controls.Add(this.lbl_rm);
+            this.Controls.Add(this.lbl_income);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_income;
+        private System.Windows.Forms.Label lbl_rm;
+        private System.Windows.Forms.Label lbl_electricity;
+        private System.Windows.Forms.Label lbl_hvi;
+        private System.Windows.Forms.Label lbl_water;
+        private System.Windows.Forms.Label lbl_ct;
+        private System.Windows.Forms.Label lbl_mr;
+        private System.Windows.Forms.Label lbl_pc;
+        private System.Windows.Forms.Label lbl_cs;
+        private System.Windows.Forms.Label lbl_parking;
+        private System.Windows.Forms.Label lbl_fuel;
+        private System.Windows.Forms.Label lbl_food;
+        private System.Windows.Forms.Label lbl_entertainment;
+        private System.Windows.Forms.Label lbl_sr;
+        private System.Windows.Forms.Label lbl_travel;
+        private System.Windows.Forms.Label Gifts;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.Label lbl_other;
     }
 }
 
